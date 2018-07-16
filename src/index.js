a0bc290e71a0170bc9574287b9db8da6aaa85f1b
Ryan Johnson <ryan@shakainteractive.com>
2018-07-16 14:51:24 -0400

    create basic element

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+const element = <h1>Hello World!</h1>;
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(element, rootElement);
