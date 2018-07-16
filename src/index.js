36f3ab8937b0f81f5f905c54ccaf0899540d7611
Ryan Johnson <ryan@shakainteractive.com>
2018-07-16 16:41:46 -0400

    change App component to class component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App(props) {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox: {props.version}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox: {this.props.version}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
