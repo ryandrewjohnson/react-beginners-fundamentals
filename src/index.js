37955aaf0bf6a72b87b4287ca67616362d15a7f3
Ryan Johnson <ryan@shakainteractive.com>
2018-07-17 14:08:49 -0400

    step 2

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App(props) {
+  let joke = "Here is original joke!";
+
+  const onTellJoke = () => {
+    joke = "Here is my awesome new joke!";
+    console.log("joke", joke);
+  };
+
   return (
     <div className="App">
       <h1>Joke Generator</h1>
       <p>Click the button below to generate joke!</p>
-      <button>Tell me a joke</button>
+      <button onClick={onTellJoke}>Tell me another joke</button>
+
+      <p>{joke}</p>
     </div>
   );
 }
