39b8a46157baf4fa6c16ddbf4b9eaad7a69f8781
Ryan Johnson <ryan@shakainteractive.com>
2018-07-17 16:05:28 -0400

    step 5

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ class App extends React.Component {
     this.onTellJokeClicked = this.onTellJokeClicked.bind(this);
   }
 
+  componentDidMount() {
+    fetch(
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
+    )
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+
   onTellJokeClicked() {
     this.setState(
       {
