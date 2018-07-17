522e1cfaca514d3920493e6b3480fe1cd55522c5
Ryan Johnson <ryan@shakainteractive.com>
2018-07-17 10:41:25 -0400

    add img src attribute as string

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import "./styles.css";
 function App(props) {
   return (
     <div className="App" tabIndex="1">
+      <img
+        src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png"
+        width="200"
+        alt="logo"
+      />
       <h1>Hello CodeSandbox: {props.version}</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
