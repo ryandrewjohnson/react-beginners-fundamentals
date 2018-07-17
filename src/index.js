02232a482b2ff584f489e28d6728500c2708f923
Ryan Johnson <ryan@shakainteractive.com>
2018-07-17 13:41:33 -0400

    step 1

--- src/index.js
+++ src/index.js
@@ -3,29 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const logoUrl =
-  "https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png";
-
-const myHtml = "<strong>Strong will not be rendered!</strong>";
-
 function App(props) {
   return (
-    <div className="App" tabIndex="1">
-      <img src={logoUrl} width="200" alt="logo" />
-      <h1>Hello CodeSandbox: {props.version}</h1>
-
-      {props.version < 1 ? (
-        <p>You should upgrade your version</p>
-      ) : (
-        <p>Your version is up to date</p>
-      )}
-
-      <p>{myHtml}</p>
-
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App">
+      <h1>Joke Generator</h1>
+      <p>Click the button below to generate joke!</p>
+      <button>Tell me a joke</button>
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App version="1.0" />, rootElement);
+ReactDOM.render(<App />, rootElement);
