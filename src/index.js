dcc5dfd525f8c8357a609be7b49e99eb4e12d66b
Ryan Johnson <ryan@shakainteractive.com>
2018-08-05 21:13:59 -0400

    step 1 (new)

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import "./styles.css";
 function App(props) {
   return (
     <div className="App">
-      <h1>Joke Generator</h1>
-      <p>Click the button below to generate joke!</p>
-      <button>Tell me a joke</button>
+      <button onClick={() => console.log('clicked')}>Tell me a joke</button>
     </div>
   );
 }
