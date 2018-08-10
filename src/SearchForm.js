800a4840ab59746d0cd021446dba68cf1dda927c
Ryan Johnson <ryan@shakainteractive.com>
2018-08-09 21:51:48 -0400

    step 39 new

---
+++ src/SearchForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+ const JokeSearchForm = props => <h1>Hello from Joke Form!</h1>;
+
+ export default JokeSearchForm;
--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ class App extends React.Component {
         <form onSubmit={this.onSearchSubmit}>
           <input type="text" placeholder="Enter search term..." onChange={this.onSearchChange} />
           <button>Search</button>
-          <button onClick={() => this.searchJokes(1)} disabled={this.state.isFetchingJoke}>Tell me a joke</button>
+          <button onClick={() => this.searchJokes(1)} disabled={this.state.isFetchingJoke}>I'm Feeling Funny 🤪</button>
         </form>
 
         {this.state.isFetchingJoke 
