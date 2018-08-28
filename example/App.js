291161c98ce4479027952cf04c4f0c2d3525e9cb
Ryan Johnson <ryan@shakainteractive.com>
2018-08-27 21:45:32 -0400

    update steps

---
+++ example/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Body from 'Body';
+import Footer from 'Footer';
+
+const App = props => (
+  <main>
+    <Header />
+    <Body />
+    <Footer />
+  </main>
+);
---
+++ example/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AwesomeButton from 'AwesomeButton';
+
+const Header = props => (
+  <header>
+    <h1>Welcome to my tutorial!</h1>
+    <p>Interseting stuff should go here.</p>
+    <AwesomeButton />
+  </header>
+);
+
+export default Header;
